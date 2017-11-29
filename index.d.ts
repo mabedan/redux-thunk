1790f7b279449f50c470b42996deedcabdc6c57f
mabedan <mabedan@gmail.com>
2017-11-29 10:35:50 +0100

    change extraArgument to optional in index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -2,7 +2,7 @@ import {Middleware, Dispatch} from "redux";
 
 
 export type ThunkAction<R, S, E> = (dispatch: Dispatch<S>, getState: () => S,
-                                    extraArgument: E) => R;
+                                    extraArgument?: E) => R;
 
 declare module "redux" {
   export interface Dispatch<S> {
